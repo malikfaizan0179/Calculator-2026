4ce47dfbdcc5aff61baf7121e08bc7ff67cb0f74
malikfaizan0179 <malikfaizan0179@gmail.com>
2026-09-17 15:10:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Calculator-2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
